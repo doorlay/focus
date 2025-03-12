2c245fd094f3df8947e1d59cbc834b457e2c52ad
Nic Doorlay <ndoorlay@gmail.com>
2025-03-11 20:24:46 -0700

    Finish blocklist UI

--- src/blocklist/blocklist.js
+++ src/blocklist/blocklist.js
@@ -1,3 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
-// List is put into an element with id "blocklist-text"
+document.addEventListener("DOMContentLoaded", function (){
+    // Identify relevant DOM areas by their ID
+    const blockedWebsitesTextArea = document.getElementById("blocklist-text")
+    const saveButton = document.getElementById("blocklist-button")
+
+    // Populate the blocklist text area with the domains the user wants blocked
+    const blockedWebsites = localStorage.getItem("blockedWebsites")
+    if (blockedWebsites != null){
+        console.log("in here");
+        blockedWebsitesTextArea.value = blockedWebsites.split(",").join("\n");
+    }
+   
+    // When the save button is clicked, store blocklist websites in local browser storage
+    saveButton.addEventListener("click", function() {
+        console.log("button clicked!");
+        const blockedWebsites = blockedWebsitesTextArea.value.split("\n");
+        localStorage.setItem("blockedWebsites", blockedWebsites);
+    });
+});
